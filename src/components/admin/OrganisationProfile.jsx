945ed998162ddf1f5c320a86a0bd21a7b3291b74
Adebisi01 <oluwafemiadebisi01@gmail.com>
2024-08-08 00:13:59 +0100

    1,2,3,4,7,8 fixes

--- src/components/admin/OrganisationProfile.jsx
+++ src/components/admin/OrganisationProfile.jsx
@@ -143,29 +143,25 @@ console.log(userData)
 
 
   return (
-   <div className="pb-[6rem]">
-        <p className='mb-[0.5rem] font-[600] '>Organisation profile details</p>
+    <div className="pb-[6rem]">
+      <p className="mb-[0.5rem] font-[600] ">Organisation profile details</p>
 
-        
-
-          <form encType='multipart/form-data' onSubmit={handleSubmit}>
-          <div className="grid md:grid-cols-[340px_340px] gap-x-[5rem] gap-y-[1rem]">
-                <TextField
-                   label="Organisation name"
-                    value={userData.name}
-                   // onClick={''}
-                   type="text"
-                   paddingY="py-[0.5rem]"
-                   fontSize="font-[500] bg-grey-500 cursor-not-allowed"
-                   textSize="text-[0.875rem] "
-                   onChange={handleOnChange}
-                   name="name"
-                   errMsg={capitalizeFirstLetter(errMsg.name) }
-                   disabled={true}
-
-
-                />
-                {/* <TextField
+      <form encType="multipart/form-data" onSubmit={handleSubmit}>
+        <div className="grid md:grid-cols-[340px_340px] gap-x-[5rem] gap-y-[1rem]">
+          <TextField
+            label="Organisation name"
+            value={userData.name}
+            // onClick={''}
+            type="text"
+            paddingY="py-[0.5rem]"
+            fontSize="font-[500] bg-grey-500 cursor-not-allowed"
+            textSize="text-[0.875rem] "
+            onChange={handleOnChange}
+            name="name"
+            errMsg={capitalizeFirstLetter(errMsg.name)}
+            disabled={true}
+          />
+          {/* <TextField
                    label="Industry"
                    value={userData.industry_id}
                   
@@ -176,49 +172,44 @@ console.log(userData)
                    textSize="text-[0.875rem]"
 
                 /> */}
-                <SelectField
-                label="Industry"
-                options={inDustryNames}
-                key1={"code"}
-                key2={"name"}
-                selectedOption={userData.industry_id}
-                onChange={handleOnChange}
-                name="industry_id"
-                errMsg={capitalizeFirstLetter(errMsg.industry_id) }
-                paddingY="py-[0.5rem]"
-                fontSize="font-[400] text-[0.85rem]"
-               />
+          <SelectField
+            label="Industry"
+            options={inDustryNames}
+            key1={"code"}
+            key2={"name"}
+            selectedOption={userData.industry_id}
+            onChange={handleOnChange}
+            name="industry_id"
+            errMsg={capitalizeFirstLetter(errMsg.industry_id)}
+            paddingY="py-[0.5rem]"
+            fontSize="font-[400] text-[0.85rem]"
+          />
 
-                
-                <TextField
-                   label="Organisation email"
-                   value={userData.email}
-                  
-                   // onClick={''}
-                   type="text"
-                   paddingY="py-[0.5rem]"
-                   fontSize="font-[500]"
-                   textSize="text-[0.875rem]"
-                   name="email"
-                   onChange={handleOnChange}
-                   errMsg={capitalizeFirstLetter(errMsg.email) }
-
-                />
-                <TextField
-                   label="Official organisation address"
-                   value={userData.address}
-                  
-                   // onClick={''}
-                   type="text"
-                   name="address"
-                   paddingY="py-[0.5rem]"
-                   fontSize="font-[500]"
-                   textSize="text-[0.875rem]"
-                   onChange={handleOnChange}
-                   errMsg={capitalizeFirstLetter(errMsg.address) }
-
-                />
-                {/* <TextField
+          <TextField
+            label="Organisation email"
+            value={userData.email}
+            // onClick={''}
+            type="text"
+            paddingY="py-[0.5rem]"
+            fontSize="font-[500]"
+            textSize="text-[0.875rem]"
+            name="email"
+            onChange={handleOnChange}
+            errMsg={capitalizeFirstLetter(errMsg.email)}
+          />
+          <TextField
+            label="Official organisation address"
+            value={userData.address}
+            // onClick={''}
+            type="text"
+            name="address"
+            paddingY="py-[0.5rem]"
+            fontSize="font-[500]"
+            textSize="text-[0.875rem]"
+            onChange={handleOnChange}
+            errMsg={capitalizeFirstLetter(errMsg.address)}
+          />
+          {/* <TextField
                    label="Organization location"
                   
                   
@@ -231,7 +222,7 @@ console.log(userData)
                   //  onChange={handleOnChange}
 
                 /> */}
-                {/* <TextField
+          {/* <TextField
                    label="Organisation phone Number"
                   //  value={userData.phone_no}
                    name="phone_no"
@@ -247,145 +238,141 @@ console.log(userData)
                    
 
                 /> */}
-                <TextField
-                   label="Organisation website"
-                   value={userData.website}
-                   name="website"
-                   // onClick={''}
-                   type="text"
-                   paddingY="py-[0.5rem]"
-                   fontSize="font-[500]"
-                   textSize="text-[0.875rem]"
-                   onChange={handleOnChange}
-                   errMsg={capitalizeFirstLetter(errMsg.website) }
+          <TextField
+            label="Organisation website"
+            value={userData.website}
+            name="website"
+            // onClick={''}
+            type="text"
+            paddingY="py-[0.5rem]"
+            fontSize="font-[500]"
+            textSize="text-[0.875rem]"
+            onChange={handleOnChange}
+            errMsg={capitalizeFirstLetter(errMsg.website)}
+          />
+          <TextField
+            label="HR Personnel's name"
+            value={userData.director_name}
+            name="director_name"
+            // onClick={''}
+            type="text"
+            paddingY="py-[0.5rem]"
+            fontSize="font-[500]"
+            textSize="text-[0.875rem]"
+            onChange={handleOnChange}
+            errMsg={capitalizeFirstLetter(errMsg.director_name)}
+          />
+          <TextField
+            label="HR Personnel’s phone Number"
+            value={userData.director_phone_number}
+            name="director_phone_number"
+            // onClick={''}
+            type="text"
+            paddingY="py-[0.5rem]"
+            fontSize="font-[500]"
+            textSize="text-[0.875rem]"
+            onChange={handleOnChange}
+            errMsg={capitalizeFirstLetter(errMsg.director_phone_number)}
+          />
+          <TextField
+            label="Staff number"
+            value={userData.number_of_staff}
+            name="number_of_staff"
+            // onClick={''}
+            type="text"
+            paddingY="py-[0.5rem]"
+            fontSize="font-[500]"
+            textSize="text-[0.875rem]"
+            onChange={handleOnChange}
+            errMsg={capitalizeFirstLetter(errMsg.number_of_staff)}
+          />
 
-
-                />
-                <TextField
-                   label="CEO's name"
-                   value={userData.director_name}
-                   name="director_name"
-                   // onClick={''}
-                   type="text"
-                   paddingY="py-[0.5rem]"
-                   fontSize="font-[500]"
-                   textSize="text-[0.875rem]"
-                   onChange={handleOnChange}
-                   errMsg={capitalizeFirstLetter(errMsg.director_name) }
-
-
-                />
-                <TextField
-                   label="CEO’s phone Number"
-                   value={userData.director_phone_number}
-                   name="director_phone_number"
-                   // onClick={''}
-                   type="text"
-                   paddingY="py-[0.5rem]"
-                   fontSize="font-[500]"
-                   textSize="text-[0.875rem]"
-                   onChange={handleOnChange}
-                   errMsg={capitalizeFirstLetter(errMsg.director_phone_number) }
-
-
-                />
-                <TextField
-                   label="Staff number"
-                   value={userData.number_of_staff}
-                     name="number_of_staff"
-                   // onClick={''}
-                   type="text"
-                   paddingY="py-[0.5rem]"
-                   fontSize="font-[500]"
-                   textSize="text-[0.875rem]"
-                   onChange={handleOnChange}
-                   errMsg={capitalizeFirstLetter(errMsg.number_of_staff) }
-
-                />
-
-                <div className="relative">
-                <TextField
-                   label="Lowest paid salary to staff"
-                   value={userData.lowest_paid_salary}
-                   name="lowest_paid_salary"
-                   // onClick={''}
-                   type="text"
-                   paddingY="py-[0.5rem]"
-                   fontSize="font-[500]"
-                   textSize="text-[0.875rem]"
-                   onChange={handleOnChange}
-                   errMsg={capitalizeFirstLetter(errMsg.lowest_paid_salary) }
-
-
-                />
-                  <p className='absolute right-[10%] top-[53%] text-[0.8rem]'>per month</p>
-                </div>
-
-                <div className="relative">
- 
-                <TextField
-                   label="Highest paid salary to staff"
-                   value={userData.highest_paid_salary}
-                     name="highest_paid_salary"
-                   // onClick={''}
-                   type="text"
-                   paddingY="py-[0.5rem]"
-                   fontSize="font-[500]"
-                   textSize="text-[0.875rem]"
-                   onChange={handleOnChange}
-                   errMsg={capitalizeFirstLetter(errMsg.highest_paid_salary) }
-
-
-                />
-
-<p className='absolute right-[10%] top-[53%] text-[0.8rem]'>per month</p>
-                </div>
-               
+          <div className="relative">
+            <TextField
+              label="Lowest paid salary to staff"
+              value={userData.lowest_paid_salary}
+              name="lowest_paid_salary"
+              // onClick={''}
+              type="text"
+              paddingY="py-[0.5rem]"
+              fontSize="font-[500]"
+              textSize="text-[0.875rem]"
+              onChange={handleOnChange}
+              errMsg={capitalizeFirstLetter(errMsg.lowest_paid_salary)}
+            />
+            <p className="absolute right-[10%] top-[53%] text-[0.8rem]">
+              per month
+            </p>
           </div>
 
+          <div className="relative">
+            <TextField
+              label="Highest paid salary to staff"
+              value={userData.highest_paid_salary}
+              name="highest_paid_salary"
+              // onClick={''}
+              type="text"
+              paddingY="py-[0.5rem]"
+              fontSize="font-[500]"
+              textSize="text-[0.875rem]"
+              onChange={handleOnChange}
+              errMsg={capitalizeFirstLetter(errMsg.highest_paid_salary)}
+            />
 
-      <div className="mt-[2rem] relative">
-        <p>Logo</p>
-
-       
-        <input type="file" onChange={handlePreviewImage} className='logo invisible absolute' />
-        
-        
-      <div onClick={()=>document.querySelector('.logo').click()} className={`rounded-[4px] hover:cursor-pointer  ${userData.logo != ''? '':'  justify-center h-[180px]  items-center' } mt-[0.4rem] text-[#6B7280] flex   flex-col max-w-[160px] w-full  border-2`}>
-           
-           {
-            userData.logo != '' ? 
-            <div className="flex ">
-                  <img src={userImage?userImage: userData.logo}  alt="" />
-                  {/* <img src={ userImage}  alt="" /> */}
-            </div>
-
-     
-            :
-
-            <>
-             <span className="text-[3rem] ">
-            <SlCloudUpload />
-            </span>
-            <p>Upload logo</p>
-            </>
-           }
-           
-           
+            <p className="absolute right-[10%] top-[53%] text-[0.8rem]">
+              per month
+            </p>
           </div>
-      </div>
-      <div className="">
-              {
-                        errMsg.success ?  <p>{errMsg.success}</p> : errMsg.error && <p>{errMsg.error}</p>
-              }
-         </div>
-      <div className="mt-[4rem]">
-           <InnerButton type="submit" text={isPending? "Loading...": "Update profile" } width="w-fit"   />
-      </div>
-         
-          </form>
+        </div>
 
-      
-   </div>
-  )
+        <div className="mt-[2rem] relative">
+          <p>Logo</p>
+
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handlePreviewImage}
+            className="logo invisible absolute"
+          />
+
+          <div
+            onClick={() => document.querySelector(".logo").click()}
+            className={`rounded-[4px] hover:cursor-pointer  ${
+              userData.logo != ""
+                ? ""
+                : "  justify-center h-[180px]  items-center"
+            } mt-[0.4rem] text-[#6B7280] flex   flex-col max-w-[160px] w-full  border-2`}
+          >
+            {userData.logo != "" ? (
+              <div className="flex ">
+                <img src={userImage ? userImage : userData.logo} alt="" />
+                {/* <img src={ userImage}  alt="" /> */}
+              </div>
+            ) : (
+              <>
+                <span className="text-[3rem] ">
+                  <SlCloudUpload />
+                </span>
+                <p>Upload logo</p>
+              </>
+            )}
+          </div>
+        </div>
+        <div className="">
+          {errMsg.success ? (
+            <p>{errMsg.success}</p>
+          ) : (
+            errMsg.error && <p>{errMsg.error}</p>
+          )}
+        </div>
+        <div className="mt-[4rem]">
+          <InnerButton
+            type="submit"
+            text={isPending ? "Loading..." : "Update profile"}
+            width="w-fit"
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/pages/admin/FormTwoOnboard.jsx
+++ src/pages/admin/FormTwoOnboard.jsx
@@ -88,108 +88,122 @@ const handleSubmit =  (e)=>{
 
   return (
     <div className="h-[90vh] md:h-[66vh]">
-          <div className='bg-white  mt-[1rem] rounded-[4px] px-[1rem] md:px-[2rem] py-[2rem]'>
+      <div className="bg-white  mt-[1rem] rounded-[4px] px-[1rem] md:px-[2rem] py-[2rem]">
         <div className="flex justify-between items-center text-[0.9rem]">
-            <h1 className='font-[600]'>Organisation Information</h1>
-            <p className='text-[var(--primary-color)] uppercase'>Step 2 / 2</p>
+          <h1 className="font-[600]">Organisation Information</h1>
+          <p className="text-[var(--primary-color)] uppercase">Step 2 / 2</p>
         </div>
 
+        <form onSubmit={handleSubmit}>
+          <div className="mt-[3rem]">
+            <div className="md:grid  gap-x-[1rem] grid-cols-2 ">
+              <div className="md:my-0 my-[1rem]">
+                <TextField
+                  label="The founder / HR personnel’s  name"
+                  fontSize="text-[0.75rem] font-[600] w-full "
+                  type="text"
+                  paddingY="py-[0.6rem]"
+                  name="director_name"
+                  defaultValue={data.director_name}
+                  onChange={handleOnChange}
+                  errMsg={
+                    errMsgs.director_name &&
+                    capitalizeFirstLetter(errMsgs.director_name)
+                  }
 
-        <form onSubmit={handleSubmit } >
-        <div className="mt-[3rem]">
-              <div className="md:grid  gap-x-[1rem] grid-cols-2 ">
-                <div className="md:my-0 my-[1rem]">
-                <TextField 
-                        label="The founder / CEO’s  name"
-                        fontSize="text-[0.75rem] font-[600] w-full "
-                        type="text"
-                        paddingY="py-[0.6rem]"
-                        name="director_name"
-                        defaultValue={data.director_name}
-                        onChange={handleOnChange}
-                        errMsg={errMsgs.director_name && capitalizeFirstLetter(errMsgs.director_name)}
-                        
-                        // value="General Manager"
-                        // errMsg="hjkkj"
-
-                      />
-                </div>
-                 
-
-                <div className="md:my-0 my-[1rem]">
-                <TextField 
-                        label="The CEO / founder’s phone number"
-                        fontSize="text-[0.75rem] font-[600] w-full"
-                        type="text"
-                        paddingY="py-[0.6rem]"
-                        name="director_phone_number"
-                        defaultValue={data.director_phone_number}
-                        onChange={handleOnChange}
-                        errMsg={errMsgs.director_phone_number && capitalizeFirstLetter(errMsgs.director_phone_number)}
-                        // value="General Manager"
-                        // errMsg="hjkkj"
-
-                      />
-                </div>
-                 
+                  // value="General Manager"
+                  // errMsg="hjkkj"
+                />
               </div>
-              <div className="md:grid gap-x-[1rem] grid-cols-2 md:my-[2rem]">
 
-                
-                 
-
-                 <div className="md:my-0 my-[1rem]  relative">
-                 <TextField 
-                        label="Lowest paid salary to staff"
-                        fontSize="text-[0.75rem] font-[600] w-full"
-                        type="text"
-                        paddingY="py-[0.6rem]"
-                        name="lowest_paid_salary"
-                        // defaultValue={"₦"+ data.lowest_paid_salary +".00"}
-                        defaultValue={data.lowest_paid_salary}
-                        onChange={handleOnChange}
-                        errMsg={errMsgs.lowest_paid_salary && capitalizeFirstLetter(errMsgs.lowest_paid_salary)}
-                        // defaultValue={ data.lowest_paid_salary}
-                        // errMsg="hjkkj"
-
-                      />
-                      <p className='absolute right-[10%] top-[53%] text-[0.8rem]'>per month</p>
-                 </div>
-                  
-                 <div className="relative">
-                    <TextField 
-                              label="Highest paid salary to staff"
-                              fontSize="text-[0.75rem] font-[600] w-full"
-                              type="text"
-                              paddingY="py-[0.6rem]"
-                              name="highest_paid_salary"
-                              // defaultValue={"₦"+ data.highest_paid_salary+".00"}
-                              defaultValue={data.highest_paid_salary}
-                              onChange={handleOnChange}
-                              errMsg={errMsgs.highest_paid_salary && capitalizeFirstLetter(errMsgs.highest_paid_salary)}
-                              // defaultValue={data.highest_paid_salary}
-                              // errMsg="hjkkj"
-
-                            />
-                       <p className='absolute right-[10%] top-[53%] text-[0.8rem]'>per month</p>
+              <div className="md:my-0 my-[1rem]">
+                <TextField
+                  label="The HR personnel's / founder’s phone number"
+                  fontSize="text-[0.75rem] font-[600] w-full"
+                  type="text"
+                  paddingY="py-[0.6rem]"
+                  name="director_phone_number"
+                  defaultValue={data.director_phone_number}
+                  onChange={handleOnChange}
+                  errMsg={
+                    errMsgs.director_phone_number &&
+                    capitalizeFirstLetter(errMsgs.director_phone_number)
+                  }
+                  // value="General Manager"
+                  // errMsg="hjkkj"
+                />
               </div>
+            </div>
+            <div className="md:grid gap-x-[1rem] grid-cols-2 md:my-[2rem]">
+              <div className="md:my-0 my-[1rem]  relative">
+                <TextField
+                  label="Lowest paid salary to staff"
+                  fontSize="text-[0.75rem] font-[600] w-full"
+                  type="text"
+                  paddingY="py-[0.6rem]"
+                  name="lowest_paid_salary"
+                  // defaultValue={"₦"+ data.lowest_paid_salary +".00"}
+                  defaultValue={data.lowest_paid_salary}
+                  onChange={handleOnChange}
+                  errMsg={
+                    errMsgs.lowest_paid_salary &&
+                    capitalizeFirstLetter(errMsgs.lowest_paid_salary)
+                  }
+                  // defaultValue={ data.lowest_paid_salary}
+                  // errMsg="hjkkj"
+                />
+                <p className="absolute right-[10%] top-[53%] text-[0.8rem]">
+                  per month
+                </p>
               </div>
-              
-          <div className="mt-[1rem] mb-[1rem]">
+
+              <div className="relative">
+                <TextField
+                  label="Highest paid salary to staff"
+                  fontSize="text-[0.75rem] font-[600] w-full"
+                  type="text"
+                  paddingY="py-[0.6rem]"
+                  name="highest_paid_salary"
+                  // defaultValue={"₦"+ data.highest_paid_salary+".00"}
+                  defaultValue={data.highest_paid_salary}
+                  onChange={handleOnChange}
+                  errMsg={
+                    errMsgs.highest_paid_salary &&
+                    capitalizeFirstLetter(errMsgs.highest_paid_salary)
+                  }
+                  // defaultValue={data.highest_paid_salary}
+                  // errMsg="hjkkj"
+                />
+                <p className="absolute right-[10%] top-[53%] text-[0.8rem]">
+                  per month
+                </p>
+              </div>
+            </div>
+
+            <div className="mt-[1rem] mb-[1rem]">
               {/* <Button type="submit" text={isPending? "Loading...": "Save & Proceed"} padding="py-[0.6rem]" /> */}
               {/* <Button type="submit" text={isPending? "Loading...": "Save & Submit"} padding="py-[0.6rem]" /> */}
-              <Button type="submit" text={isPending? "Loading...": "Next"} padding="py-[0.6rem]" />
-             
+              <Button
+                type="submit"
+                text={isPending ? "Loading..." : "Next"}
+                padding="py-[0.6rem]"
+              />
+            </div>
+            <div className="flex justify-center items-center">
+              <button
+                type="button"
+                onClick={() => navigate("/onboarding")}
+                className="text-[#374151] flex items-center"
+              >
+                <span className="">
+                  <MdArrowBackIos />
+                </span>{" "}
+                Previous Step
+              </button>
+            </div>
           </div>
-          <div className="flex justify-center items-center">
-              <button type="button" onClick={()=>navigate('/onboarding')} className='text-[#374151] flex items-center'><span className=""><MdArrowBackIos /></span> Previous Step</button>
-          </div>
-             
-        </div>
         </form>
-  </div>
-
+      </div>
     </div>
-    
-  )
+  );
 }
